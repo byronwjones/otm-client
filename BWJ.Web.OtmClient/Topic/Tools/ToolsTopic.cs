3e9acb76aa9b9d2bc4883531fdd1ee669b0d30e7
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2021-10-23 18:19:12 -0400

    Create Tools API topic

---
+++ BWJ.Web.OtmClient/Topic/Tools/ToolsTopic.cs
@@ -0,0 +1,17 @@
+﻿using BWJ.Web.OTM.Http;
+
+namespace BWJ.Web.OTM.Topic.Tools
+{
+    public sealed class ToolsTopic
+    {
+        private readonly OtmHttpClient _client;
+
+        internal ToolsTopic(OtmHttpClient client)
+        {
+            _client = client;
+            Admin = new AdminTopic(_client);
+        }
+
+        public AdminTopic Admin { get; }
+    }
+}
