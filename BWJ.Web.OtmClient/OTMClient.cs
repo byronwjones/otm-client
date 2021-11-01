9ffbf13f2bd0b4e3e7e9646854c1d717e2de6cf7
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2021-11-01 10:23:25 -0400

    Create OTM client

---
+++ BWJ.Web.OtmClient/OTMClient.cs
@@ -0,0 +1,21 @@
+﻿using BWJ.Web.OTM.Http;
+using BWJ.Web.OTM.Topic;
+using BWJ.Web.OTM.Topic.Tools;
+
+namespace BWJ.Web.OTM
+{
+    public sealed class OTMClient
+    {
+        private readonly OtmHttpClient client = new OtmHttpClient();
+        public OTMClient()
+        {
+            Authentication = new AuthenticationTopic(client);
+            Tools = new ToolsTopic(client);
+            Territory = new TerritoryTopic(client);
+        }
+
+        public AuthenticationTopic Authentication { get; }
+        public ToolsTopic Tools { get; }
+        public TerritoryTopic Territory { get; }
+    }
+}
